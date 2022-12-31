ae6f2faef6f22d1b92cb59b200694b712793f7b9
Keith F. Woeltje <comm@woeltje.org>
2022-12-30 20:08:37 -0600

    create proper event list

--- read_file_demo.py
+++ read_file_demo.py
@@ -4,6 +4,9 @@ events = []
 
 with open('events.txt') as f:
     for line in f:
-        events.append(line.strip().split(","))
+        l = line.strip().split(",")
+        event = (l[0], dt.date(int(l[1]),int(l[2]), int(l[3])))
+        events.append(event)
+        
 
 print(events)
